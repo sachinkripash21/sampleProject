5c235eea723480755a468f390a601620b9215bbd
sachi <sachi@LAPTOP-5SVFBQN8.Home>
2022-11-23 11:10:12 +0000

    Second Commit

--- FrameworkPractice/src/test/java/sachinbhandary/Tests/CartPageFunctionalityTest.java
+++ FrameworkPractice/src/test/java/sachinbhandary/Tests/CartPageFunctionalityTest.java
@@ -15,53 +15,53 @@ import sachinbhandary.Testcomponents.BaseTest;
 
 public class CartPageFunctionalityTest extends BaseTest{
 	
-//	@Test(dataProvider="getData")
-//	public void RefreshCartTest(HashMap<String,String> input) throws InterruptedException
-//	{
-//		ProductPage productPage = landingPage.loginApplication(input.get("username"), input.get("password"));
-//		List<WebElement> products =productPage.getProducts();
-//		productPage.getProductByName(input.get("itema"));
-//		productPage.addProductToCart(input.get("itema"));
-//		CartPage cartPage = productPage.gotoCartPage();
-//		driver.navigate().refresh();
-//		cartPage.getProducts();
-//		Thread.sleep(3);
-//		boolean match = cartPage.verifyProducts(input.get("itema"));				
-//		Assert.assertTrue(match);
-//		Thread.sleep(3);
-//	}
-//	
-//	@Test(dataProvider="getData")
-//	public void LogoutAndVerifyCartProduct(HashMap<String,String> input) throws InterruptedException
-//	{
-//		ProductPage productPage = landingPage.loginApplication(input.get("username"), input.get("password"));
-//		List<WebElement> products =productPage.getProducts();
-//		productPage.getProductByName(input.get("itema"));
-//		productPage.addProductToCart(input.get("itema"));
-//		Thread.sleep(3);
-//		productPage.clickLogout();
-//		Thread.sleep(3);
-//		landingPage.loginApplication(input.get("username"), input.get("password"));
-//		Thread.sleep(3);
-//		CartPage cartPage = productPage.gotoCartPage();
-//		cartPage.getProducts();
-//		boolean match = cartPage.verifyProducts(input.get("itema"));				
-//		Assert.assertTrue(match);
-//	}
-//	
-//	@Test(dataProvider="getData")
-//	public void VerifyRemoveButton(HashMap<String,String> input)
-//	{
-//		ProductPage productPage = landingPage.loginApplication(input.get("username"), input.get("password"));
-//		List<WebElement> products =productPage.getProducts();
-//		productPage.getProductByName(input.get("itema"));
-//		productPage.addProductToCart(input.get("itema"));
-//		CartPage cartPage = productPage.gotoCartPage();
-//		cartPage.getProducts();
-//		cartPage.removeButtonClick();
-//		boolean match = cartPage.itemsNotRemoved();
-//		Assert.assertFalse(match);
-//	}
+	@Test(dataProvider="getData")
+	public void RefreshCartTest(HashMap<String,String> input) throws InterruptedException
+	{
+		ProductPage productPage = landingPage.loginApplication(input.get("username"), input.get("password"));
+		List<WebElement> products =productPage.getProducts();
+		productPage.getProductByName(input.get("itema"));
+		productPage.addProductToCart(input.get("itema"));
+		CartPage cartPage = productPage.gotoCartPage();
+		driver.navigate().refresh();
+		cartPage.getProducts();
+		Thread.sleep(3);
+		boolean match = cartPage.verifyProducts(input.get("itema"));				
+		Assert.assertTrue(match);
+		Thread.sleep(3);
+	}
+	
+	@Test(dataProvider="getData")
+	public void LogoutAndVerifyCartProduct(HashMap<String,String> input) throws InterruptedException
+	{
+		ProductPage productPage = landingPage.loginApplication(input.get("username"), input.get("password"));
+		List<WebElement> products =productPage.getProducts();
+		productPage.getProductByName(input.get("itema"));
+		productPage.addProductToCart(input.get("itema"));
+		Thread.sleep(3);
+		productPage.clickLogout();
+		Thread.sleep(3);
+		landingPage.loginApplication(input.get("username"), input.get("password"));
+		Thread.sleep(3);
+		CartPage cartPage = productPage.gotoCartPage();
+		cartPage.getProducts();
+		boolean match = cartPage.verifyProducts(input.get("itema"));				
+		Assert.assertTrue(match);
+	}
+	
+	@Test(dataProvider="getData")
+	public void VerifyRemoveButton(HashMap<String,String> input)
+	{
+		ProductPage productPage = landingPage.loginApplication(input.get("username"), input.get("password"));
+		List<WebElement> products =productPage.getProducts();
+		productPage.getProductByName(input.get("itema"));
+		productPage.addProductToCart(input.get("itema"));
+		CartPage cartPage = productPage.gotoCartPage();
+		cartPage.getProducts();
+		cartPage.removeButtonClick();
+		boolean match = cartPage.itemsNotRemoved();
+		Assert.assertFalse(match);
+	}
 	
 	@Test(dataProvider="getData")
 	public void RemoveAllItemsFromCart(HashMap<String,String> input)
